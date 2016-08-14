7fe046ded1a8c6f49ae3497730cbe6a1cd31bb69
Jason Gray <jasonlevigray3@gmail.com>
2016-08-14 09:54:23 -0500

    Update NotifyExample.py

--- NotifyExample.py
+++ NotifyExample.py
@@ -8,17 +8,21 @@ class NotificationsDemo(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title='Notifications Demo')
         self.supports_actions = False
+        self.cute_animals = ''
         self.init_notifications()
         self.init_ui()
 
     def init_ui(self):
         self.set_border_width(10)
 
-        hbox = Gtk.Box(spacing=6)
-        self.add(hbox)
-        button = Gtk.Button.new_with_label('Ask the most import of all questions...')
-        button.connect('clicked', self.on_question_clicked)
-        hbox.pack_start(button, True, True, 0)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+        self.label = Gtk.Label()
+        vbox.pack_start(self.label, True, True, 0)
+        self.label.set_label('Notifications are Asynchronous.')
+        self.button = Gtk.Button.new_with_label('Ask the most import of all questions...')
+        self.button.connect('clicked', self.on_question_clicked)
+        vbox.pack_start(self.button, True, True, 0)
 
     def init_notifications(self):
         def on_init_finish(caps):
@@ -40,6 +44,10 @@ class NotificationsDemo(Gtk.Window):
             body = 'I guess we will never know if you prefer Puppies or Kittens.'
             icon = 'dialog-error'
         self.notification.new(summary, body, icon)
+        if self.cute_animals:
+            self.label.set_label('You preferred {} last time.'.format(self.cute_animals))
+        else:
+            self.label.set_label('They will not block the main thread.')
 
     def set_actions(self):
         self.notification.clear_actions()
@@ -54,17 +62,23 @@ class NotificationsDemo(Gtk.Window):
         )
 
     def puppies_cb(self):
-        self.you_prefer('Puppies')
+        self.cute_animals = 'Puppies'
+        self.label.set_label('So you\'ll more than likely never see this.')
+        self.you_prefer()
 
     def kittens_cb(self):
-        self.you_prefer('Kittens')
+        self.cute_animals = 'Kittens'
+        self.label.set_label('So you\'ll more than likely never see this.')
+        self.you_prefer()
 
-    def you_prefer(self, cute_animals):
+    def you_prefer(self):
         self.notification.clear_actions()
         summary = 'You have made your choice.'
-        body = 'You prefer {}.'.format(cute_animals)
+        body = 'You prefer {}.'.format(self.cute_animals)
         icon = 'dialog-information'
         self.notification.new(summary, body, icon)
+        self.button.set_label('Ask Again...')
+        self.label.set_label('Are you sure you prefer {}?'.format(self.cute_animals))
 
 if __name__ == '__main__':
     win = NotificationsDemo()
