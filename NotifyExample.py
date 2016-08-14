f07826953fa25e814fb5eea688fa7a42ef14b71a
Jason Gray <jasonlevigray3@gmail.com>
2016-08-14 12:01:18 -0500

    Mix the animals up a bit just for fun

--- NotifyExample.py
+++ NotifyExample.py
@@ -1,20 +1,24 @@
+import random
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from SimpleDBusNotifications import *
 
 class NotificationsDemo(Gtk.Window):
-
     def __init__(self):
         Gtk.Window.__init__(self, title='Notifications Demo')
         self.supports_actions = False
-        self.cute_animals = ''
+        self.animal_one = ''
+        self.animal_two = ''
+        self.selected_animal = ''
+        self.animals = ['Puppies', 'Kittens', 'Cows', 'Horses', 'Ponies', 'Dragons', 'Unicorns',
+                        'Baby Seals', 'Whales', 'Dolphins', 'Sharks', 'Gold Fish', 'Manticores',
+                        'Zebras', 'Lions', 'Tigers', 'Bears', 'Aardvarks', 'Panthers', 'Camels']
         self.init_notifications()
         self.init_ui()
 
     def init_ui(self):
         self.set_border_width(10)
-
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         self.add(vbox)
         self.label = Gtk.Label()
@@ -30,6 +34,20 @@ class NotificationsDemo(Gtk.Window):
 
         self.notification = SimpleDBusNotifications.async_init('Notifications Demo', on_init_finish)
 
+    def set_animals(self):
+        prev_animal_one = self.animal_one
+        for i in range(len(self.animals)):
+            animal = random.choice(self.animals)
+            if animal != self.animal_one and animal != self.animal_two:
+                self.animal_one = animal
+                break
+
+        for i in range(len(self.animals)):
+            animal = random.choice(self.animals)
+            if animal != self.animal_one and animal != self.animal_two and animal != prev_animal_one:
+                self.animal_two = animal
+                break
+
     def on_question_clicked(self, button):
         self.send_first_notification()
 
@@ -41,47 +59,48 @@ class NotificationsDemo(Gtk.Window):
             icon = 'dialog-question'
         else:
             summary = 'Your notification server does not support actions.' 
-            body = 'I guess we will never know if you prefer Puppies or Kittens.'
+            body = 'I guess we will never know which animals you prefer.'
             icon = 'dialog-error'
         self.notification.new(summary, body, icon)
-        if self.cute_animals:
-            self.label.set_label('You preferred {} last time.'.format(self.cute_animals))
+        if self.selected_animal:
+            self.label.set_label('You preferred {} last time.'.format(self.selected_animal))
         else:
             self.label.set_label('They will not block the main thread.')
 
     def set_actions(self):
         self.notification.clear_actions()
-        self.notification.add_action('action-id-puppies', 
-                                     'Puppies',
-                                     self.puppies_cb,
+        self.set_animals()
+        self.notification.add_action('action-id-animal-one', 
+                                     self.animal_one,
+                                     self.animal_one_cb,
         )
 
-        self.notification.add_action('action-id-kittens',
-                                     'Kittens',
-                                     self.kittens_cb,
+        self.notification.add_action('action-id-animal-two',
+                                     self.animal_two,
+                                     self.animal_two_cb,
         )
 
-    def puppies_cb(self):
-        self.cute_animals = 'Puppies'
+    def animal_one_cb(self):
+        self.selected_animal = self.animal_one
         self.label.set_label('So you\'ll more than likely never see this.')
         self.you_prefer()
 
-    def kittens_cb(self):
-        self.cute_animals = 'Kittens'
+    def animal_two_cb(self):
+        self.selected_animal = self.animal_two
         self.label.set_label('So you\'ll more than likely never see this.')
         self.you_prefer()
 
     def you_prefer(self):
         self.notification.clear_actions()
         summary = 'You have made your choice.'
-        body = 'You prefer {}.'.format(self.cute_animals)
+        body = 'You prefer {}.'.format(self.selected_animal)
         icon = 'dialog-information'
         # If notifications were not async(non-blocking)
         # you'd be able to see the label from the callbacks
         # until it unblocked.
         self.notification.new(summary, body, icon)
         self.button.set_label('Ask Again...')
-        self.label.set_label('Are you sure you prefer {}?'.format(self.cute_animals))
+        self.label.set_label('Are you sure you prefer {}?'.format(self.selected_animal))
 
 if __name__ == '__main__':
     win = NotificationsDemo()
