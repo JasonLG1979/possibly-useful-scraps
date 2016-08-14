d5019421ad255c5f6cb468497a13ba48d781b90c
Jason Gray <jasonlevigray3@gmail.com>
2016-08-14 06:35:55 -0500

    Update NotifyExample.py

--- NotifyExample.py
+++ NotifyExample.py
@@ -1,4 +1,4 @@
-
+import time
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -8,7 +8,7 @@ class NotifyExample:
     def __init__(self):
         self.notification = None
         self.supports_actions = False
-        self.quit_on_close = False
+        self.cute_animals = 'Neither'
         self.on_init()
 
     def on_init(self):
@@ -22,13 +22,9 @@ class NotifyExample:
     def g_signal_handler(self, notification, sender_name, signal_name, parameters):
         id, signal_value = parameters.unpack()
         if id != notification.id:#we only care about our notification
-           return
+            return
         if signal_name == 'NotificationClosed':
-            self.maybe_quit()
-
-    def maybe_quit(self):
-        if self.quit_on_close:
-            Gtk.main_quit()   
+            self.you_prefer()  
 
     def set_notification(self):
         if self.supports_actions:
@@ -41,7 +37,6 @@ class NotifyExample:
             body = 'I guess we will never know if you prefer puppies or kittens.'
             icon = 'dialog-error'
         self.notification.new(summary, body, icon)
-        self.quit_on_close = True
 
     def set_actions(self):
         self.notification.add_action('action-id-puppies', 
@@ -55,20 +50,22 @@ class NotifyExample:
         )
 
     def puppies_cb(self):
-        self.quit_on_close = False
-        self.you_prefer('Puppies')
+        self.cute_animals = 'Puppies'
 
     def kittens_cb(self):
-        self.quit_on_close = False
-        self.you_prefer('Kittens')
+        self.cute_animals = 'Kittens'
 
-    def you_prefer(self, cute_animals):
-        self.notification.clear_actions()
-        summary = 'You have made your choice.'
-        body = 'You prefer {}.'.format(cute_animals)
-        icon = 'dialog-information'
-        self.notification.new(summary, body, icon)
-        self.quit_on_close = True
+    def you_prefer(self):
+        if not self.supports_actions:
+            Gtk.main_quit()
+        else:
+            self.notification.clear_actions()
+            summary = 'You have made your choice.'
+            body = 'You prefer {}.'.format(self.cute_animals)
+            icon = 'dialog-information'
+            self.notification.new(summary, body, icon)
+            time.sleep(1)#give the call a chance to do it's thing
+            Gtk.main_quit()
 
 if __name__ == '__main__':
     app = NotifyExample()
