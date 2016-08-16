8157511b33e56915cbabe55de5d0ad71b69f6cfe
Jason Gray <jasonlevigray3@gmail.com>
2016-08-15 21:03:46 -0500

    Update AsyncExample.py

--- AsyncExample.py
+++ AsyncExample.py
@@ -1,3 +1,4 @@
+
 import time
 import random
 import gi
@@ -10,87 +11,77 @@ class GLibAsyncDemo(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title='GLib_async Demo')
         self.ui_loop_timer_id = 0
-        self.time_sec = 1000
-        self.init_ui()
-        self.init_notifications()
-
-    def init_ui(self):
+        self.time_sec = 0
+        self.set_default_size(350, -1)
+        self.set_resizable(False)
         self.set_border_width(10)
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.headerbar = Gtk.HeaderBar()
+        self.headerbar.set_show_close_button(True)
+        self.headerbar.set_title('Async Demo')
+        self.headerbar.set_subtitle('00:00:00')
+        self.set_titlebar(self.headerbar)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
         self.add(vbox)
-        self.label = Gtk.Label()
-        vbox.pack_start(self.label, True, True, 0)
-        self.label.set_label('00:00')
-        self.button = Gtk.Button.new_with_label('Reset Clock')
-        self.button.connect('clicked', self.reset_clock)
-        vbox.pack_start(self.button, True, True, 0)
+        self.button = Gtk.Button.new_with_label('Start Clock')
+        self.button.connect('clicked', self.clock_toggle)
+        vbox.pack_start(self.button, False, False, 0)
+        self.notification = SimpleDBusNotifications.async_init('Async Demo', self.init_notifications_finish)
 
-    def init_notifications(self):
-        def on_init_finish(caps):
-            self.async_sleep()
-            self.create_ui_loop()
-
-        self.notification = SimpleDBusNotifications.async_init('GLib_async Demo', on_init_finish)
+    def init_notifications_finish(self, caps):         
+        self.async_sleep()
+        self.clock_toggle()
 
     def update_time(self):
         self.time_sec += 1
-        self.label.set_label(self.format_time(self.time_sec))
+        time_int = self.time_sec
+        s = time_int % 60
+        time_int //= 60
+        m = time_int % 60
+        time_int //= 60
+        h = time_int
+        self.headerbar.set_subtitle('{:02d}:{:02d}:{:02d}'.format(h, m, s))
         return True
 
-    def reset_clock(self, *ignore):
-        # The display clock is totally independent of async_sleep
-        # It continues to tick away even though async_sleep spends
-        # most of it's time sleeping unless you reset it... 
-        self.destroy_ui_loop()
-        self.create_ui_loop()
-
-    def format_time(self, milliseconds):
-        s, ms = divmod(milliseconds, 1000)
-        m, s = divmod(s, 60)
-        h, m = divmod(m, 60)
-        if h:
-            return '{:02d}:{:02d}:{:02d}:{:03d}'.format(h, m, s, ms)
-        else:
-            return '{:02d}:{:02d}:{:03d}'.format(m, s, ms)
-
-    def async_sleep(self):
-        def wake_back_up(result, error):
-            if error:
-                print(error)
-                return
-            self.async_sleep()
-            self.send_wake_notification(result)
-
-        @GLib_async_func(on_done=wake_back_up, PRIORITY=GLib.PRIORITY_HIGH)
-        def go_to_sleep(sleep_time):
-            # If this sleep were done in the main thread
-            # the clock would stop.
-            went_to_sleep = time.time()
-            time.sleep(sleep_time)
-            woke_up = time.time()
-            return '{:6.3f}'.format(round(woke_up - went_to_sleep, 3))
-
-        go_to_sleep(random.uniform(5.0, 10.0))
-
-    def create_ui_loop(self):
-        if not self.ui_loop_timer_id:
-            self.ui_loop_timer_id = GLib.timeout_add(1, self.update_time)
-
-    def destroy_ui_loop(self):
+    def clock_toggle(self, *ignore): 
         if self.ui_loop_timer_id:
             GLib.source_remove(self.ui_loop_timer_id)
             self.ui_loop_timer_id = 0
             self.time_sec = 0
-            self.label.set_label('00:00')
+            self.headerbar.set_subtitle('00:00:00')
+            self.button.set_label('Start Clock')
+        else:
+            self.ui_loop_timer_id = GLib.timeout_add(1000, self.update_time)
+            self.button.set_label('Reset Clock')
+
+    def async_sleep(self):
+        # When the work is done the callback recieves
+        # the result or error in the main thread.
+        def wake_back_up(result, error):
+            if error:
+                print(error)
+                return
+            self.send_wake_notification(result)
+
+                         #async callback       #priority at which the main thread is re-entered.
+        @GLib_async_func(on_done=wake_back_up, PRIORITY=GLib.PRIORITY_LOW)
+        # Work done in seperate thread.
+        def go_to_sleep(sleep_time):
+            went_to_sleep = time.time()
+            time.sleep(sleep_time)
+            woke_up = time.time()
+            return round(woke_up - went_to_sleep)
+
+        go_to_sleep(random.randint(5, 15))
 
     def send_wake_notification(self, secs):
         summary = 'Just Woke Back Up.'
-        body = 'Slept for about {} seconds.'.format(secs)
+        body = 'Slept for about {} seconds that time.'.format(secs)
         icon = 'dialog-information'
         self.notification.new(summary, body, icon)
+        self.async_sleep()
 
 if __name__ == '__main__':
     win = GLibAsyncDemo()
-    win.connect("delete-event", Gtk.main_quit)
+    win.connect('delete-event', Gtk.main_quit)
     win.show_all()
     Gtk.main()
