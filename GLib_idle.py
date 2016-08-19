a205c5892c176137756823162d187fdac13e2210
Jason Gray <jasonlevigray3@gmail.com>
2016-08-18 22:28:38 -0500

    Create GLib_idle.py

---
+++ GLib_idle.py
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2016 Jason Gray <jasonlevigray3@gmail.com>
+#
+#This program is free software: you can redistribute it and/or modify it 
+#under the terms of the GNU General Public License version 3, as published 
+#by the Free Software Foundation.
+#
+#This program is distributed in the hope that it will be useful, but 
+#WITHOUT ANY WARRANTY; without even the implied warranties of 
+#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#PURPOSE.  See the GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along 
+#with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+from gi.repository import GLib
+
+def GLib_idle(f):
+    def wrapper(*args, **kwargs):
+        GLib.idle_add(lambda: f(*args, **kwargs))
+    return wrapper
